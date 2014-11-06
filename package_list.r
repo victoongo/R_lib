9792cd233cad711b952425a0f0b3731c31e6732f
Victor Wang <victoongo@gmail.com>
2014-11-06 15:40:37 -0500

    added files from Ilyas

--- package_list.r
+++ package_list.r
@@ -1,15 +1,26 @@
+# CPU
 install.packages('doParallel')
 
+# GPU
+#install.packages('WideLM') # older R only
+#install.packages('gputools')
+
+# Presenting Info
 install.packages('xtable')
 install.packages('R2HTML')
+install.packages('Shiny')
+install.packages('knitr')
 
+# Viz
 install.packages('ggplot2')
 install.packages('qqman')
-install.packages('mhtplot2')
+install.packages('mhtplot2') # older R only
 
+# Data Mining
 install.packages('rpart')
 install.packages('party')
 install.packages('DMwR')
 install.packages('GGally')
 
-install.packages('knitr')
+# Machine Learning
+install.packages('randomForest')
